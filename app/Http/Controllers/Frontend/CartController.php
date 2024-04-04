3e7f85088a67b6115993e8adce82bae158caac7c
Aniruddha7890 <ani.mjumdar@gmail.com>
2024-04-04 23:32:15 +0530

    added the product in the cart through sesion

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\ProductVariantItem;
 use Illuminate\Http\Request;
+use Cart;
 
 class CartController extends Controller
 {
@@ -13,21 +14,38 @@ class CartController extends Controller
     public function addToCart(Request $request){
         $product = Product::findOrFail($request->product_id);
         $variants = [];
-        foreach ($request->variant_items as $item_id) {
-            $variantItem = ProductVariantItem::findOrFail($item_id);
-            $variants[$variantItem->productVariant->name]['name'] = $variantItem->name;
-            $variants[$variantItem->productVariant->name]['price'] = $variantItem->price;
+
+        $variantTotalAmount = 0;
+
+        if($request->has('variant_items')){
+            foreach ($request->variant_items as $item_id) {
+                $variantItem = ProductVariantItem::findOrFail($item_id);
+                $variants[$variantItem->productVariant->name]['name'] = $variantItem->name;
+                $variants[$variantItem->productVariant->name]['price'] = $variantItem->price;
+                $variantTotalAmount += $variantItem->price;
+            }
         }
         
+        //check discount
+        $productTotalAmount = 0;
+        if(checkDiscount($product)){
+            $productTotalAmount = ($product->offer_price + $variantTotalAmount);
+        } else{
+            $productTotalAmount = ($product->price + $variantTotalAmount);
+        }
+
         $cartData = [];
         $cartData['id'] = $product->id;
         $cartData['name'] = $product->name;
         $cartData['qty'] = $request->qty;
-        $cartData['price'] = $product->price * $request->qty;
+        $cartData['price'] = $productTotalAmount;
         $cartData['weight'] = 10;
         $cartData['options']['variants'] = $variants;
+        $cartData['options']['image'] = $product->thumb_image;
+        $cartData['options']['slug'] = $product->slug;
 
-        dd($cartData);
-        //Cart::add();
+        Cart::add($cartData);
+
+        return response(['status' => 'success', 'message' => 'Added to cart successfully!']);
     }
 }
