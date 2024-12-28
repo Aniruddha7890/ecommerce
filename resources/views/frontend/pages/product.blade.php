acf74c92ed3e7cc932c84c7a4e3155c0ddf55b14
Aniruddha7890 <ani.mjumdar@gmail.com>
2024-12-28 22:15:09 +0530

    corrected the sesion of the product showing type through session

--- resources/views/frontend/pages/product.blade.php
+++ resources/views/frontend/pages/product.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
     <!--============================
-                                                                                BREADCRUMB START
-                                                                            ==============================-->
+                                                                                                                        BREADCRUMB START
+                                                                                                                    ==============================-->
     <section id="wsus__breadcrumb">
         <div class="wsus_breadcrumb_overlay">
             <div class="container">
@@ -24,13 +24,13 @@
         </div>
     </section>
     <!--============================
-                                                                                BREADCRUMB END
-                                                                            ==============================-->
+                                                                                                                        BREADCRUMB END
+                                                                                                                    ==============================-->
 
 
     <!--============================
-                                                                                PRODUCT PAGE START
-                                                                            ==============================-->
+                                                                                                                        PRODUCT PAGE START
+                                                                                                                    ==============================-->
     <section id="wsus__product_page">
         <div class="container">
             <div class="row">
@@ -241,12 +241,16 @@
                                 <div class="wsus__product_topbar_left">
                                     <div class="nav nav-pills" id="v-pills-tab" role="tablist"
                                         aria-orientation="vertical">
-                                        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill"
+                                        <button
+                                            class="nav-link {{ session()->has('product_list_style') && session()->get('product_list_style') == 'grid' ? 'active' : '' }} {{ !session()->has('product_list_style') ? 'active' : '' }} list-view"
+                                            data-id="grid" id="v-pills-home-tab" data-bs-toggle="pill"
                                             data-bs-target="#v-pills-home" type="button" role="tab"
                                             aria-controls="v-pills-home" aria-selected="true">
                                             <i class="fas fa-th"></i>
                                         </button>
-                                        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill"
+                                        <button
+                                            class="nav-link {{ session()->has('product_list_style') && session()->get('product_list_style') == 'list' ? 'active' : '' }} list-view"
+                                            data-id="list" id="v-pills-profile-tab" data-bs-toggle="pill"
                                             data-bs-target="#v-pills-profile" type="button" role="tab"
                                             aria-controls="v-pills-profile" aria-selected="false">
                                             <i class="fas fa-list-ul"></i>
@@ -273,8 +277,8 @@
                             </div>
                         </div>
                         <div class="tab-content" id="v-pills-tabContent">
-                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
-                                aria-labelledby="v-pills-home-tab">
+                            <div class="tab-pane fade {{ session()->has('product_list_style') && session()->get('product_list_style') == 'grid' ? 'show active' : '' }} {{ !session()->has('product_list_style') ? 'show active' : '' }}"
+                                id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                                 <div class="row">
                                     @foreach ($products as $product)
                                         <div class="col-xl-4  col-sm-6">
@@ -351,8 +355,8 @@
 
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel"
-                                aria-labelledby="v-pills-profile-tab">
+                            <div class="tab-pane fade {{ session()->has('product_list_style') && session()->get('product_list_style') == 'list' ? 'show active' : '' }}"
+                                id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
                                 <div class="row">
                                     @foreach ($products as $product)
                                         <div class="col-xl-12">
@@ -455,6 +459,24 @@
         </div>
     </section>
     <!--============================
-                                                                                PRODUCT PAGE END
-                                                                            ==============================-->
+                                                                                                                        PRODUCT PAGE END
+                                                                                                                    ==============================-->
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('.list-view').on('click', function() {
+                let style = $(this).data('id');
+
+                $.ajax({
+                    method: 'GET',
+                    url: "{{ route('change-product-list-view') }}",
+                    data: {
+                        style: style
+                    },
+                })
+            })
+        })
+    </script>
+@endpush
