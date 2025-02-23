eb2e2d236f9ebd6a179f423aeaff90b6a3f981bd
Aniruddha7890 <ani.mjumdar@gmail.com>
2025-02-23 23:24:19 +0530

    upto mail sent and verification done

--- app/Http/Controllers/Frontend/NewsletterController.php
+++ app/Http/Controllers/Frontend/NewsletterController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Mail\SubscriptionVerification;
+use App\Models\NewsletterSubscriber;
 use Illuminate\Http\Request;
+use App\Helper\MailHelper;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class NewsletterController extends Controller
 {
@@ -12,5 +17,51 @@ class NewsletterController extends Controller
         $request->validate([
             'email' => ['required', 'email']
         ]);
+
+        $existSubscriber = NewsletterSubscriber::where('email', $request->email)->first();
+
+        if (!empty($existSubscriber)) {
+            if ($existSubscriber->is_verified == 0) {
+                $existSubscriber->verified_token = Str::random(25);
+                $existSubscriber->save();
+                // set mail config
+                MailHelper::setMailConfig();
+                // send mail
+                Mail::to($existSubscriber->email)->send(new SubscriptionVerification($existSubscriber));
+
+                return response(['status' => 'success', 'message' => 'A verification link has been sent to your email, please check']);
+            } elseif ($existSubscriber->is_verified == 1) {
+                return response(['status' => 'error', 'message' => 'You already subscribed with this email!']);
+            }
+        } else {
+            $subscriber = new NewsletterSubscriber();
+            $subscriber->email = $request->email;
+            $subscriber->verified_token = Str::random(25);
+            $subscriber->is_verified = 0;
+            $subscriber->save();
+
+            //set config
+            MailHelper::setMailConfig();
+
+            /// send mail
+            Mail::to($subscriber->email)->send(new SubscriptionVerification($subscriber));
+
+            return response(['status' => 'success', 'message' => 'A verification link has been sent to your email, please check']);
+        }
+    }
+
+    public function newsLetterEmailVerify($token)
+    {
+        $verify = NewsletterSubscriber::where('verified_token', $token)->first();
+        if ($verify) {
+            $verify->verified_token = 'verified';
+            $verify->is_verified = 1;
+            $verify->save();
+            toastr("Email verification successfull", "success", "Success");
+            return redirect()->route('home');
+        } else {
+            toastr("Invalid token", "error", "Error");
+            return redirect()->route('home');
+        }
     }
 }
