10e40c8a6c80784d7ac46e13fb212db5ba0a0340
Aniruddha7890 <ani.mjumdar@gmail.com>
2025-03-01 17:13:22 +0530

    Implement newsletter email sending functionality and create newsletter mail template, site footer section done fully

--- app/Http/Controllers/Backend/SubscribersController.php
+++ app/Http/Controllers/Backend/SubscribersController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Backend;
 
 use App\DataTables\NewsletterSubscriberDataTable;
 use App\Http\Controllers\Controller;
+use App\Mail\Newsletter;
+use App\Helper\MailHelper;
 use App\Models\NewsletterSubscriber;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class SubscribersController extends Controller
 {
@@ -14,17 +17,27 @@ class SubscribersController extends Controller
         return $datatable->render('admin.subscriber.index');
     }
 
-    public function destroy(string $Id)
-    {
-        $subscriber = NewsletterSubscriber::findOrFail($Id)->delete();
-        return response(['status' => 'success', 'message' => 'Subscriber deleted successfully']);
-    }
-
     public function sendMail(Request $request)
     {
         $request->validate([
             'subject' => ['required'],
             'message' => ['required'],
         ]);
+
+        $emails = NewsletterSubscriber::where('is_verified', 1)->pluck('email')->toArray();
+
+        // set mail config
+        MailHelper::setMailConfig();
+
+        Mail::to($emails)->send(new Newsletter($request->subject, $request->message));
+        toastr('Mail has been sent', 'success', 'Success');
+
+        return redirect()->back();
+    }
+
+    public function destroy(string $Id)
+    {
+        $subscriber = NewsletterSubscriber::findOrFail($Id)->delete();
+        return response(['status' => 'success', 'message' => 'Subscriber deleted successfully']);
     }
 }
